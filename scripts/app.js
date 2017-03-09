a1c0309c22ac08d9c216b1fd40a545b4e57a1ab5
Traci Williams <ug3000@gmail.com>
2017-03-08 20:35:23 -0600

    first chat working

--- scripts/app.js
+++ scripts/app.js
@@ -1,3 +1,16 @@
 window.onload = function() {
 	console.log("old school chatroom connected")
 }
+
+
+function enterComment(){
+	var userComment = document.getElementById("comments").value;
+	var yourUsername = document.getElementById("yourUsername").value;
+
+	var text = yourUsername + " says " + userComment;
+
+	var displayContent = document.getElementById("content");
+
+	displayContent.innerHTML = text;
+
+}
